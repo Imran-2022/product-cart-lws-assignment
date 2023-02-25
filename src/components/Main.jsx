4ac1b0f0dbb49a56e67c5d50f7ab190d63ffc48d
Imran-2022 <mdimranulhaque.202@gmail.com>
2023-02-25 15:15:03 +0600

    showing in product page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import CreateProducts from './CreateProducts';
 import ProductContainer from './ProductContainer';
 
 const Main = () => {
+
+    const product = useSelector(store => store.products)
+    console.log(product);
+
     return (
         <div>
             <main class="py-16">
                 <div class="productWrapper">
-                    <ProductContainer />
+                    <div class="productContainer" id="lws-productContainer">
+
+                        {
+                            product?.map(dt => <ProductContainer dt={dt} key={dt.product_id} />)
+                        }
+                    </div>
                     <CreateProducts />
                 </div>
             </main>
--- src/components/ProductContainer.jsx
+++ src/components/ProductContainer.jsx
@@ -1,38 +1,22 @@
 import React from 'react';
 
-const ProductContainer = () => {
-    return (
-        <div class="productContainer" id="lws-productContainer">
-        {/* <!-- product item --> */}
-        <div class="lws-productCard">
-          <img class="lws-productImage" src="https://i.dummyjson.com/data/products/59/thumbnail.jpg" alt="product" />
-          <div class="p-4 space-y-2">
-            <h4 class="lws-productName">Spring and summershoes</h4>
-            <p class="lws-productCategory">Mens shoes</p>
-            <div class="flex items-center justify-between pb-2">
-              <p class="productPrice">BDT <span class="lws-price">400</span></p>
-              <p class="productQuantity">QTY <span class="lws-quantity">10</span></p>
-            </div>
-            <button class="lws-btnAddToCart">Add To Cart</button>
-          </div>
-        </div>
-        {/* <!-- product item ends --> */}
+const ProductContainer = ({dt}) => {
+  const {product_id, product_name, product_category, product_image_url, product_price, product_quantity } = dt;
 
-        {/* <!-- product item --> */}
+    return (
         <div class="lws-productCard">
-          <img class="lws-productImage" src="https://i.dummyjson.com/data/products/40/thumbnail.jpg" alt="product" />
+          <img class="lws-productImage" src={product_image_url} alt="product" />
           <div class="p-4 space-y-2">
-            <h4 class="lws-productName">Women Winter Clothes</h4>
-            <p class="lws-productCategory">Tops</p>
+            <h4 class="lws-productName">{product_name}</h4>
+            <p class="lws-productCategory">{product_category}</p>
             <div class="flex items-center justify-between pb-2">
-              <p class="productPrice">BDT <span class="lws-price">100</span></p>
-              <p class="productQuantity">QTY <span class="lws-quantity">30</span></p>
+              <p class="productPrice">BDT <span class="lws-price">{product_price}</span></p>
+              <p class="productQuantity">QTY <span class="lws-quantity">{product_quantity}</span></p>
             </div>
             <button class="lws-btnAddToCart">Add To Cart</button>
           </div>
         </div>
-        {/* <!-- product item ends --> */}
-      </div>
+    
     );
 };
 
